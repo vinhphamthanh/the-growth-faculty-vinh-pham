4f08a83d9648d36fa5d493701f37112fcc9a1c38
Vinh Pham <vinh.phamthanh16@gmail.com>
2024-04-05 10:17:09 +0700

    chore: update workflow

--- utils/greeting.sh
+++ utils/greeting.sh
@@ -2,3 +2,4 @@
 
 echo "This is script to run when the workflow wants to reference!"
 echo "Nice to be here GITHUB"
+echo "I want to use the global scope variable in this script. Greeting! $DEV_NAME"
